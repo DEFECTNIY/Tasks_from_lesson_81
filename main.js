3e254d2a82d98c63fadbe9e0f0eb133c0b636c2c
DEFECTNIY <145113063+DEFECTNIY@users.noreply.github.com>
2023-11-27 20:02:44 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+// 1 задание
+const numbers=[1,2,3,4,5,6,7,8,9,10,12]
+
+for(let i = 0; i < numbers.length; i++){
+  if (numbers[i]%2===0)
+  console.log(numbers[i])
+}
+
+// 2
+let colors = ["red", "orange", "yellow", "green", "blue", "purple"];
+
+for (let i = colors.length - 1; i >= 0; i--) {
+  console.log(colors[i]);
+}
